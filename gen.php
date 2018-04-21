63cebe8f54923566e28f6e81ddbf24d52b478464
LaurieGreen <lauriegreen18@gmail.com>
2018-04-21 15:51:52 +0100

    pre merge commit

---
+++ gen.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+<title>Laurie's Page</title>
+</head>
+<body>
+
+<div>
+    <img id="gifholder" src="http://i.imgur.com/pg5qRqS.gif"></br>
+    <button id="read">New Gif</button>
+</div>
+
+<script>
+    $("#read").click(function(){
+        $.get("read.php", function(data, status){   
+        // do stuff with data        
+            $('#gifholder').attr("src", data);
+        });
+    });
+</script>
+
+</body>
+</html>
