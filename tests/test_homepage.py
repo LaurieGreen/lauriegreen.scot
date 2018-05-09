20ca3f7893ef52b236c243d10e0e2b79bfcf6982
LaurieGreen <lauriegreen18@gmail.com>
2018-05-09 22:38:03 +0100

    adding testing

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -7,11 +7,9 @@ def test_title():
     driver.get('http://lauriegreen.scot/')
     driver.maximize_window()
     assert "Laurie Green" in driver.title
-    driver.close()
     
 
 def test_header():
     driver.get('http://lauriegreen.scot/')
     driver.maximize_window()
     assert driver.find_element_by_css_selector('h1').text == "Hello World"
-    driver.close()
