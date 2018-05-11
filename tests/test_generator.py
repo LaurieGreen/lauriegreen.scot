6512bc08691069d3978c999fc197119be08c6571
Laurie Green <lauriegreen18@gmail.com>
2018-05-11 14:19:27 +0100

    initial move

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,15 +1,5 @@
 import pytest;
 
-@pytest.fixture()
-def setup_driver():
-	from selenium import webdriver
-	from selenium.webdriver.chrome.options import Options
-	chrome_options = Options()
-	chrome_options.add_argument("--headless")
-	driver = webdriver.Chrome(chrome_options=chrome_options)
-	yield driver
-	driver.close()
-
 def test_title(setup_driver):
     setup_driver.get('http://lauriegreen.scot/')
     setup_driver.maximize_window()
