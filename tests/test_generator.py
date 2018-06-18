7698674ee74eb667cf34c0918650e4161ef0a56b
LaurieGreen <lauriegreen18@gmail.com>
2018-06-18 17:21:33 +0100

    some more gen tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,18 +1,15 @@
 import pytest;
 
-def test_title(setup_driver):
-    setup_driver.get('http://lauriegreen.scot/')
-    setup_driver.maximize_window()
-    assert "Laurie Green" in setup_driver.title
-    
-def test_header(setup_driver):
-    setup_driver.get('http://lauriegreen.scot/')
-    setup_driver.maximize_window()
-    assert setup_driver.find_element_by_css_selector('h1').text == "Hello World"
-
-def test_open_gen(setup_driver):
+def test_opening_gen_from_homepage(setup_driver):
     setup_driver.get('http://lauriegreen.scot/')
     setup_driver.maximize_window()
     assert setup_driver.find_element_by_css_selector('a[href="gen.html"').text == "Generator"
     setup_driver.find_element_by_css_selector('a[href="gen.html"').click()
     assert setup_driver.current_url == 'http://lauriegreen.scot/gen.html'
+
+def test_gif_button(setup_driver):
+ 	setup_driver.get('http://lauriegreen.scot/gen.html')
+	setup_driver.maximize_window()
+	assert setup_driver.find_element_by_css_selector('img[id="gifholder"').get_attribute("src") == "http://i.imgur.com/pg5qRqS.gif"
+	setup_driver.find_element_by_css_selector('button[id="readButton"').click()
+	assert setup_driver.find_element_by_css_selector('img[id="gifholder"').get_attribute("src") != "http://i.imgur.com/pg5qRqS.gif"
